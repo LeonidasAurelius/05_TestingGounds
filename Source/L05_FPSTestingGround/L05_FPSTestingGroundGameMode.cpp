d43bfa1082e779d2c71f06648d24e0558600cc45
Jerry <jerryklu2013@gmail.com>
2018-10-19 21:59:54 +0800

    L257 Restructure files and put on gun for Patrol Guard

--- Source/L05_FPSTestingGround/L05_FPSTestingGroundGameMode.cpp
+++ Source/L05_FPSTestingGround/L05_FPSTestingGroundGameMode.cpp
@@ -3,13 +3,13 @@
 #include "L05_FPSTestingGround.h"
 #include "L05_FPSTestingGroundGameMode.h"
 #include "L05_FPSTestingGroundHUD.h"
-#include "L05_FPSTestingGroundCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 
 AL05_FPSTestingGroundGameMode::AL05_FPSTestingGroundGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
--- Source/L05_FPSTestingGround/L05_FPSTestingGroundHUD.cpp
+++ Source/L05_FPSTestingGround/L05_FPSTestingGroundHUD.cpp
@@ -9,7 +9,7 @@
 AL05_FPSTestingGroundHUD::AL05_FPSTestingGroundHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshiarTexObj.Object;
 }
 
